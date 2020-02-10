c2d37ca6ec1f6652e0f2f032cfe488575ebb9f1b
garima-sindhwani26 <59004710+garima-sindhwani26@users.noreply.github.com>
2020-02-10 23:35:05 +0530

    Update ip

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://3.12.85.161:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://18.217.180.80:5432/postgres";
 			String username="postgres";
 			String password="";
 		
