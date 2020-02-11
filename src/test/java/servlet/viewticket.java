0ad2c1b270bda740b81412131748a3faf71648b6
garima-sindhwani26 <59004710+garima-sindhwani26@users.noreply.github.com>
2020-02-11 10:50:35 +0530

    Update ip

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -16,7 +16,7 @@ public class viewticket extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://18.217.180.80:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://18.217.189.80:5432/postgres";
 			String username="postgres";
 			String password="";
 						
